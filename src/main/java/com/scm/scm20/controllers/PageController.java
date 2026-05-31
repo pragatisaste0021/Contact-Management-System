e9bd741dddf83488a0dab0cab799df35cd63fa98
pragatisaste0021 <103356760+pragatisaste0021@users.noreply.github.com>
2026-05-31 22:07:39 +0530

    Update PageController.java

--- src/main/java/com/scm/scm20/controllers/PageController.java
+++ src/main/java/com/scm/scm20/controllers/PageController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import com.scm.scm20.entities.ContactQuery;
 import com.scm.scm20.entities.User;
 import com.scm.scm20.forms.ContactQueryForm;
